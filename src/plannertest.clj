2039681ffa2bd150586e1cd5b20e22d45b72eb93
Lux <q5273477@gmail.com>
2018-12-04 09:42:17 +0000

    New working planner ops, and start state

--- src/plannertest.clj
+++ src/plannertest.clj
@@ -4,47 +4,135 @@
 
 (def start-state
   '#{
-     (con S1 S2)
-     (con S2 S1)
-     (at T1 S1)
-     (at C1 S2)
+     (isa trainOne Train)
+     (tas trainOne stationOne)
+     (cas cargoOne stationTwo)
      })
 
 (def ops
 
   '{
-    :unload{:name unload
-            :achieves (at ?C ?S)
-            :when  ((on ?C ?T) (at ?T ?S))
-            :post ()
+    :unload
+            {:name unload
+            :achieves (at ?Cargo ?Station)
+            :when  ((is ?Cargo Cargo))
+            :post ( (on ?Cargo ?Train)(at ?Train ?Station))
             :pre ()
-            :del ( (on ?C ?T) )
-            :add ( (at ?C ?S) )
-            :cmd ( drop ?C )
-            :txt (?T drops ?C at ?S)
+            :del ( (on ?Cargo ?Train) )
+            :add ( (at ?Cargo ?Station) )
+            :cmd ( drop ?Cargo )
+            :txt (?T drops ?Cargo at ?Station)
             }
     :load
            {:name load
-            :achieves (at ?C ?T)
-            :when ((at ?T ?S) (at ?C ?S))
-            :post ()
+            :achieves (on ?Cargo ?Train)
+            :when ((is ?Cargo Cargo) )
+            :post ((at ?Train ?Station)(at ?Cargo ?Station))
             :pre ()
-            :del ( (at ?C ?S) )
-            :add ( (on ?C ?t) )
-            :cmd ( pickup ?C )
-            :txt (?C picked up at ?S by ?T)
+            :del ( (at ?Cargo ?Station) )
+            :add ( (on ?Cargo ?Train) )
+            :cmd ( pickup ?Cargo )
+            :txt (?C picked up at ?Station by ?Train)
             }
     :move
            {:name move
-            :achieves ((at ?T ?S2))
-            :when ((con ?S1 ?S2) (con ?S2 ?S1))
-            :post ()
+            :achieves ((at ?Train ?Station2))
+            :when ((isa ?Train Train))
+            :post ((at ?Train ?Station1))
             :pre ()
-            :del ( (at ?T ?S1) )
-            :add ( (at ?T ?S2) )
-            :cmd ( move to ?S2 )
-            :txt (?T moves to ?S2)
+            :del ( (at ?Train ?Station1))
+            :add ( (at ?Train ?Station2))
+            :cmd ( move to ?Station2 )
+            :txt (?Train moves to ?Station2)
             }
     }
   )
-;(planner start-state '(at C1 S1) ops)
+
+(def ops-alt
+
+  '{
+    :unload
+    {:name unload
+     :achieves ((cas ?Cargo ?Station) )
+     :when  ((isa ?Train Train))
+     :post ((cot ?Cargo ?Train)(tas ?Train ?Station))
+     :pre ()
+     :del ((cot ?Cargo ?Train))
+     :add ((cas ?Cargo ?Station))
+     :cmd ((drop ?Cargo))
+     :txt ((?Train drops ?Cargo at ?Station))
+     }
+    :load
+    {:name load
+     :achieves ((cot ?Cargo ?Train))
+     :when ()
+     :post ((cas ?Cargo ?Station)(tas ?Train ?Station))
+     :pre ()
+     :del ((cas ?Cargo ?Station))
+     :add ((cot ?Cargo ?Train) )
+     :cmd ((pickup ?Cargo))
+     :txt ((?Cargo picked up at ?Station by ?Train))
+     }
+    :move
+    {:name move
+     :achieves ((tas ?Train ?Dstn))
+     :when ()
+     :post ()
+     :pre ((tas ?Train ?Src))
+     :del ((tas ?Train ?Src))
+     :add ((tas ?Train ?Dstn))
+     :cmd ((move to ?Dstn) )
+     :txt ((?Train moves to ?Dstn))
+     }
+    }
+  )
+
+
+(def ss3b
+  '#{(station london)
+     (station ncl)
+     (isa ncl-exp train)
+     (tas ncl-exp london)
+     (cas llama ncl)
+     })
+
+(def ops3
+  '{
+    :unload
+    {:name unload
+     :achieves (cas ?stuff ?stn)
+     :when  ((isa ?t train))
+     :post ((cot ?stuff ?t)(tas ?t ?stn))
+     :pre ()
+     :del ( (cot ?stuff ?t))
+     :add ( (cas ?stuff ?stn) )
+     :cmd ((drop ?stuff)  )
+     :txt ((?t drops ?stuff at ?stn) )
+     }
+    :load
+    {:name load
+     :achieves (cot ?stuff ?t)
+     :when ((cas ?stuff ?stn))
+     :post ((tas ?t ?stn))
+     :pre ()
+     :del ((cas ?stuff ?stn) )
+     :add ((cot ?stuff ?t) )
+     :cmd ((pickup ?stuff))
+     :txt ((?stuff picked up at ?stn by ?t))
+     }
+    :move
+    {:name move
+     :achieves (tas ?t ?dst)
+     :when ((tas ?t ?src))
+     :post ()
+     :pre ()
+     :del ( (tas ?t ?src))
+     :add ( (tas ?t ?dst))
+     :cmd ( (move ?t to ?dst) )
+     :txt ((?t moves to ?dst))
+     }
+    }
+  )
+;(planner start-state '((cas cargoOne stationOne)) ops-alt)
+;(planner ss3b '(cas llama london) ops3)
+
