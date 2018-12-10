ff13c8ed6260630142ec919a145ce2d1ead2d40b
Aaron Walker <aaron45587@gmail.com>
2018-12-10 21:59:32 +0000

    Added new world maps

--- astarclojure/src/astarclojure/core.clj
+++ astarclojure/src/astarclojure/core.clj
@@ -28,39 +28,458 @@
     (loop [queued `((~start))
            visited nil]
 
-      (if (empty? queued) nil                               ;; fail if (null queued)
-                          (let [next (selector queued)      ;; select next node
-                                state (first next)          ;; filter out path
-                                raw-state (get-state state)
-                                ]                           ;; filter costs, etc
+      (if (empty? queued) nil             ;; fail if (null queued)
+        (let [next (selector queued)      ;; select next node
+              state (first next)          ;; filter out path
+              raw-state (get-state state)
+              ]                           ;; filter costs, etc
 
-                            (when debug (println 'selecting next '=> raw-state))
-                            (cond
-                              ;(and (fn? goal) (goal raw-state))     ;; is goal a predicate & goal found
-                              ;(reverse next)                        ;; quit with result
+          (when debug (println 'selecting next '=> raw-state))
+          (cond
+            ;(and (fn? goal) (goal raw-state))     ;; is goal a predicate & goal found
+            ;(reverse next)                        ;; quit with result
 
-                              (goal? raw-state)             ;; goal found
-                              (reverse next)                ;; quit with result
+            (goal? raw-state)             ;; goal found
+            (reverse next)                ;; quit with result
 
-                              :else
-                              (if (member? visited raw-state)
-                                (recur (remove #(= % next) queued) visited)
-                                (let [queued (remove #(= % next) queued)
-                                      moves (LMG state)
-                                      new-visited (cons raw-state visited)
-                                      new-states (map #(cons % next)
-                                                      (remove #(member? visited (get-state %))
-                                                              moves))]
+            :else
+            (if (member? visited raw-state)
+              (recur (remove #(= % next) queued) visited)
+              (let [queued (remove #(= % next) queued)
+                    moves (LMG state)
+                    new-visited (cons raw-state visited)
+                    new-states (map #(cons % next)
+                                    (remove #(member? visited (get-state %))
+                                            moves))]
 
-                                  (when debug
-                                    (println 'exploring state '=> raw-state
-                                             'path next
-                                             'moves moves))
+                (when debug
+                  (println 'exploring state '=> raw-state
+                           'path next
+                           'moves moves))
 
 
-                                  (recur
-                                    (concat queued new-states)
-                                    new-visited)))))))))
+                (recur
+                  (concat queued new-states)
+                  new-visited)))))))))
+
+
+
+;;====================== Map 1 ========================
+
+(defn map1 [state]
+
+  (let [n (:state state)
+
+        c (:cost state)
+
+        ]
+
+    (cond
+      (= n '1) (list
+
+                 {:state '2, :cost (+ c 3)}
+
+
+                 )
+      (= n '2) (list
+                 {:state '1, :cost (+ c 3)}
+
+                 {:state '3, :cost (+ c 3)}
+
+                 )
+      (= n '3) (list
+                 {:state '2, :cost (+ c 3)}
+
+                 )
+      )
+    )
+  )
+
+;;====================== Map 2 ========================
+
+(defn map2 [state]
+
+  (let [n (:state state)
+
+        c (:cost state)
+
+        ]
+
+    (cond
+      (= n '1) (list
+
+                 {:state '2, :cost (+ c 4.5)}
+
+                 {:state '3, :cost (+ c 6)}
+
+                 )
+      (= n '2) (list
+                 {:state '1, :cost (+ c 4.5)}
+
+                 {:state '3, :cost (+ c 5.7)}
+
+                 )
+      (= n '3) (list
+                 {:state '1, :cost (+ c 6)}
+
+                 {:state '2, :cost (+ c 5.7)}
+
+                 )
+      )
+    )
+  )
+
+;;====================== Map 3 ========================
+
+(defn map3 [state]
+
+  (let [n (:state state)
+
+        c (:cost state)
+
+        ]
+
+    (cond
+      (= n '1) (list
+
+                 {:state '2, :cost (+ c 4)}
+
+                 {:state '3, :cost (+ c 2.8)}
+
+                 {:state '4, :cost (+ c 4)}
+
+                 )
+      (= n '2) (list
+                 {:state '1, :cost (+ c 4)}
+
+                 {:state '3, :cost (+ c 2.8)}
+
+                 {:state '5, :cost (+ c 4)}
+
+                 )
+      (= n '3) (list
+                 {:state '1, :cost (+ c 2.8)}
+
+                 {:state '2, :cost (+ c 2.8)}
+
+                 {:state '4, :cost (+ c 2.8)}
+
+                 {:state '5, :cost (+ c 2.8)}
+
+                 )
+      (= n '4) (list
+                 {:state '1, :cost (+ c 4)}
+
+                 {:state '3, :cost (+ c 2.8)}
+
+                 {:state '5, :cost (+ c 4)}
+
+                 )
+      (= n '5) (list
+
+                 {:state '2, :cost (+ c 4)}
+
+                 {:state '3, :cost (+ c 2.8)}
+
+                 {:state '4, :cost (+ c 4)}
+
+                 )
+      )
+    )
+  )
+
+;;====================== Map 4 ========================
+
+(defn map4 [state]
+
+  (let [n (:state state)
+
+        c (:cost state)
+
+        ]
+
+    (cond
+      (= n '1) (list
+
+                 {:state '2, :cost (+ c 3.2)}
+
+                 )
+      (= n '2) (list
+                 {:state '1, :cost (+ c 3.2)}
+
+                 {:state '3, :cost (+ c 3.6)}
+
+                 {:state '4, :cost (+ c 3.6)}
+
+                 )
+      (= n '3) (list
+                 {:state '2, :cost (+ c 3.6)}
+
+                 )
+      (= n '4) (list
+                 {:state '2, :cost (+ c 3.6)}
+
+                 )
+      )
+    )
+  )
+
+;;====================== Map 5 ========================
+
+(defn map5 [state]
+
+  (let [n (:state state)
+
+        c (:cost state)
+
+        ]
+
+    (cond
+      (= n '1) (list
+
+                 {:state '2, :cost (+ c 2.8)}
+
+                 )
+      (= n '2) (list
+                 {:state '1, :cost (+ c 2.8)}
+
+                 {:state '3, :cost (+ c 3)}
+
+
+                 )
+      (= n '3) (list
+                 {:state '2, :cost (+ c 3)}
+
+                 {:state '4, :cost (+ c 3)}
+
+                 )
+      (= n '4) (list
+                 {:state '3, :cost (+ c 3)}
+
+                 {:state '5, :cost (+ c 2.8)}
+
+                 )
+      (= n '5) (list
+
+                 {:state '4, :cost (+ c 2.8)}
+
+                 )
+      )
+    )
+  )
+
+;;====================== Map 6 ========================
+
+(defn map6 [state]
+
+  (let [n (:state state)
+
+        c (:cost state)
+
+        ]
+
+    (cond
+      (= n '1) (list
+
+                 {:state '2, :cost (+ c 2)}
+
+                 )
+      (= n '2) (list
+                 {:state '1, :cost (+ c 2)}
+
+                 {:state '3, :cost (+ c 2)}
+
+
+                 )
+      (= n '3) (list
+                 {:state '2, :cost (+ c 2)}
+
+                 )
+      (= n '4) (list
+                 {:state '5, :cost (+ c 2)}
+
+                 )
+      (= n '5) (list
+
+                 {:state '4, :cost (+ c 2)}
+
+                 )
+      )
+    )
+  )
+
+;;====================== Map 7 ========================
+
+(defn map7 [state]
+
+  (let [n (:state state)
+
+        c (:cost state)
+
+        ]
+
+    (cond
+      (= n '1) (list
+
+                 {:state '1, :cost (+ c 4)}
+
+                 {:state '2, :cost (+ c 4)}
+
+                 )
+      (= n '2) (list
+                 {:state '1, :cost (+ c 4)}
+
+                 {:state '3, :cost (+ c 3)}
+
+
+                 )
+      (= n '3) (list
+                 {:state '2, :cost (+ c 3)}
+
+                 {:state '4, :cost (+ c 3)}
+
+                 )
+      (= n '4) (list
+                 {:state '3, :cost (+ c 3)}
+
+                 )
+      )
+    )
+  )
+
+;;====================== Map 8 ========================
+
+(defn map8 [state]
+
+  (let [n (:state state)
+
+        c (:cost state)
+
+        ]
+
+    (cond
+      (= n '1) (list
+
+                 {:state '2, :cost (+ c 2.2)}
+
+                 {:state '5, :cost (+ c 2.2)}
+
+                 )
+      (= n '2) (list
+                 {:state '1, :cost (+ c 2.2)}
+
+                 {:state '3, :cost (+ c 3.2)}
+
+
+                 )
+      (= n '3) (list
+                 {:state '2, :cost (+ c 3.2)}
+
+                 {:state '4, :cost (+ c 2.8)}
+
+                 )
+      (= n '4) (list
+                 {:state '3, :cost (+ c 2.8)}
+
+                 {:state '5, :cost (+ c 2)}
+
+                 )
+      (= n '5) (list
+                 {:state '1, :cost (+ c 2.2)}
+
+                 {:state '4, :cost (+ c 2)}
+
+                 )
+      )
+    )
+  )
+
+;;====================== Map 9 ========================
+
+(defn map9 [state]
+
+  (let [n (:state state)
+
+        c (:cost state)
+
+        ]
+
+    (cond
+      (= n '1) (list
+
+                 {:state '2, :cost (+ c 4.8)}
+
+                 )
+      (= n '2) (list
+                 {:state '1, :cost (+ c 4.8)}
+
+                 {:state '3, :cost (+ c 2)}
+
+
+                 )
+      (= n '3) (list
+                 {:state '2, :cost (+ c 2)}
+
+                 {:state '4, :cost (+ c 2)}
+
+                 )
+      (= n '4) (list
+                 {:state '3, :cost (+ c 2)}
+
+                 {:state '5, :cost (+ c 2.2)}
+
+                 {:state '6, :cost (+ c 2.8)}
+
+                 )
+      (= n '5) (list
+                 {:state '4, :cost (+ c 2.2)}
+
+                 )
+      (= n '6) (list
+                 {:state '4, :cost (+ c 2.8)}
+
+                 {:state '7, :cost (+ c 3.2)}
+
+                 )
+      (= n '7) (list
+                 {:state '6, :cost (+ c 3.2)}
+
+                 {:state '8, :cost (+ c 5.1)}
+
+                 )
+      (= n '8) (list
+                 {:state '7, :cost (+ c 5.1)}
+
+                 {:state '9, :cost (+ c 3.2)}
+
+                 {:state '10, :cost (+ c 2.8)}
+
+                 )
+      (= n '9) (list
+                 {:state '8, :cost (+ c 3.2)}
+
+                 )
+      (= n '10) (list
+                 {:state '8, :cost (+ c 2.8)}
+
+                 {:state '11, :cost (+ c 2.8)}
+
+                 {:state '12, :cost (+ c 3)}
+
+                 )
+      (= n '11) (list
+                 {:state '10, :cost (+ c 2.8)}
+
+                 )
+      (= n '12) (list
+                 {:state '10, :cost (+ c 3)}
+
+                 )
+      )
+    )
+  )
+
+;;====================== Testing ========================
 
 
 (defn a*lmg [state]
@@ -249,7 +668,123 @@
     )
   )
 
-;;(A*search {:state 'a, :cost 0} (fn [x] (= x 'e)) a*lmg-map)
+(defn test-A [state]
+
+  (let [n (:state state)
+
+        c (:cost state)
+
+        ]
+
+    (cond
+      (= n 's) (list
+
+                 {:state 'a, :cost (+ c 11)}
+
+                 {:state 'd, :cost (+ c 13)}
+
+                 )
+      (= n 'a) (list
+                 {:state 'b, :cost (+ c 8)}
+
+                 )
+      (= n 'b) (list
+
+                 {:state 'c, :cost (+ c 14)}
+
+                 )
+      (= n 'd) (list
+
+                 {:state 'e, :cost (+ c 10)}
+
+                 )
+      (= n 'e) (list
+
+                 {:state 'g, :cost (+ c 4)}
+
+                 )
+      (= n 'c) (list
+
+                 {:state 'g, :cost (+ c 8)}
+
+                 )
+      )
+    )
+  )
+
+(defn test-D [state]
+
+  (let [n (:state state)
+
+        c (:cost state)
+
+        ]
+
+    (cond
+      (= n 's) (list
+
+                 {:state 'a, :cost (+ c 4)}
+
+                 {:state 'e, :cost (+ c 5)}
+
+                 {:state 'f, :cost (+ c 6.4)}
+
+                 )
+      (= n 'a) (list
+                 {:state 'b, :cost (+ c 2.2)}
+
+                 )
+      (= n 'b) (list
+
+                 {:state 'c, :cost (+ c 3.6)}
+
+                 )
+      (= n 'c) (list
+
+                 {:state 'd, :cost (+ c 5)}
+
+                 )
+      (= n 'd) (list
+
+                 {:state 'c, :cost (+ c 5)}
+
+                 {:state 'end, :cost (+ c 5)}
+
+                 )
+      (= n 'e) (list
+
+                 {:state 'g, :cost (+ c 2.8)}
+
+                 )
+      (= n 'f) (list
+
+                 {:state 'g, :cost (+ c 2.8)}
+
+                 {:state 'i, :cost (+ c 2.8)}
+
+                 )
+      (= n 'g) (list
+
+                 {:state 'f, :cost (+ c 2.8)}
+
+                 {:state 'h, :cost (+ c 3.6)}
+
+                 )
+      (= n 'h) (list
+
+                 {:state 'end, :cost (+ c 4)}
+
+                 )
+      (= n 'i) (list
+
+                 {:state 'd, :cost (+ c 2.8)}
+
+                 )
+      )
+    )
+  )
+
+;;(A*search {:state 'a, :cost 0} (fn [x] (= x 'h)) a*lmg-map)
 
 (defn stateAdapter [plannerOutput]
   (let [startState (nth (clojure.string/split plannerOutput #",") 0)
